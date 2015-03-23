5e9aa7cb56736270767b1b189d2d2846f113246c
Arbiter <Arbiter@Arbiter-PC>
2015-03-24 00:26:15 +0800

    fix error about API

--- src/main/java/com/heymom/backend/controller/DeliveryAPIController.java
+++ src/main/java/com/heymom/backend/controller/DeliveryAPIController.java
@@ -5,11 +5,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.heymom.backend.dto.DeliveryDto;
 import com.heymom.backend.service.DeliveryService;
@@ -21,6 +24,8 @@ public class DeliveryAPIController {
 	private DeliveryService deliveryService;
 
 	@RequestMapping(value = "listpage", method = RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	@ResponseBody
 	public List<DeliveryDto> listActivities(@RequestParam(defaultValue = "0") int currentPage,
 			@RequestParam(defaultValue = "10") int pageSize,
 			@RequestParam(defaultValue = "createTime") String sortProperty,
@@ -31,6 +36,8 @@ public class DeliveryAPIController {
 	}
 
 	@RequestMapping(value = "list/{ids}", method = RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	@ResponseBody
 	public List<DeliveryDto> listByIds(@PathVariable String ids) {
 		String[] idArray = ids.split(",");
 		List<Integer> integerIds = new ArrayList<Integer>();
